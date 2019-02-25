e86e2419f117dfb9b284a6754f4f08dccf7ae2dd
Garrett Halstein <garrett.halstein@emory.edu>
2019-02-25 21:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine) {
   } else {
   var firstInLine = katzDeliLine.shift(); // first customer in line removed
   }
-  return `Currently serving ${firstInLine}.`;
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
 // tests for whether there is anybody in line and returns an ordered list of customers in line
